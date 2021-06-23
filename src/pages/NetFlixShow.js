b956b728e1e4597aeaf839ac76ea471a5f62dfc7
murat <muratbegalyev@gmail.com>
2021-06-23 19:38:16 +0600

    add component Banner

--- src/pages/NetFlixShow.js
+++ src/pages/NetFlixShow.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Row from "../components/Row/Row";
 import api from "../api/api";
+import Banner from "../components/Banner/Banner";
 
 const NetFlixShow = () => {
   return (
     <div>
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={api.fetchNetflixOriginals}
